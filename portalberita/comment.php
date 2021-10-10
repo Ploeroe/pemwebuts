3cdb25ed514d61a9bd2e7c8928505bbb57c08602
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2021-10-10 22:38:19 +0700

    Update Komen
    - kalo belum login redirect ke halaman login
    - kalo udah login , tidak bisa isi komen itu kosong (harus di isi)

--- portalberita/comment.php
+++ portalberita/comment.php
@@ -1,12 +1,12 @@
 <?php
 
-    $id = $_GET['id'];
+$id = $_GET['id'];
 
 ?>
 
 <div class="mainpage">
-        <div class="boxkomen mt-5 pb-5">
-            <form action="./controller/addkomen.php" method="POST">
+    <div class="boxkomen mt-5 pb-5">
+        <form action="./controller/addkomen.php" method="POST">
             <input type="hidden" name="id" value="<?php echo $id; ?>">
             <input type="hidden" name="komenid">
             <div class="me-2">
@@ -14,14 +14,14 @@
                 <hr>
                 <div class="me-2">
                     <?php
-                        include "komentar.php";
-                        ?>
-                <input class="kotakinput" type='text' name='komen' placeholder='Your comment...' id="input">
-                <button class="btnsignup mb-5" type='submit' name="comment">Comment</button>
+                    include "komentar.php";
+                    ?>
+                    <input class="kotakinput" type='text' name='komen' placeholder='Your comment...' id="input" required>
+                    <button class="btnsignup mb-5" type='submit' name="comment">Comment</button>
                 </div>
             </div>
-            </form>
-        </div>
-	<div class="clear"></div>
+        </form>
+    </div>
+    <div class="clear"></div>
 
 </div>
--- portalberita/komentar.php
+++ portalberita/komentar.php
@@ -1,85 +1,84 @@
 <div class="mainpage">
-        <?php 
-            $id = (isset($_GET['id']) ? $_GET['id'] : '');
+    <?php
+    $id = (isset($_GET['id']) ? $_GET['id'] : '');
 
-            global $connect;
+    global $connect;
 
-            $idberita = $id;
+    $idberita = $id;
 
-            $sql = mysqli_query($connect,"SELECT * FROM comment WHERE beritaid = '".$id."' ");
-            while ($komen = mysqli_fetch_array($sql)) {
-            extract($komen);
+    $sql = mysqli_query($connect, "SELECT * FROM comment WHERE beritaid = '" . $id . "' ");
+    while ($komen = mysqli_fetch_array($sql)) {
+        extract($komen);
 
-            echo' 
+        echo ' 
                 <div class="row">
                     <div class="col-1">
-                        <img class="imgkomen" src="'.$usergambar.'">
+                        <img class="imgkomen" src="' . $usergambar . '">
                     </div>
                     <div class="col-9 ms-3 mb-3">
-                        <div class="komenberita">'.$userfirst.' '.$userlast.'</div>
-                        <div class="teks-foto">'.nl2br($komentar).'</div>
-                        <div class="tglberita"> '.$Tanggal.' </div>
+                        <div class="komenberita">' . $userfirst . ' ' . $userlast . '</div>
+                        <div class="teks-foto">' . nl2br($komentar) . '</div>
+                        <div class="tglberita"> ' . $Tanggal . ' </div>
                     </div>';
-            
-            $idkomen = $id;
 
-            // $likesql = mysqli_query($connect,"SELECT * FROM hati WHERE beritaid = $idberita");
-            // $likekomen = mysqli_fetch_array($likesql);
-            var_dump($status);
-            var_dump($userid);
-            var_dump($_SESSION['userid']);
-            var_dump($komenid);
+        $idkomen = $id;
 
-            // KOMPARASI Status , userid, komenid
-            if(isset($likekomen)){
-                if($likekomen['status'] == 1 && $likekomen['userid'] === $_SESSION['userid']){
-                    echo'
+        // $likesql = mysqli_query($connect,"SELECT * FROM hati WHERE beritaid = $idberita");
+        // $likekomen = mysqli_fetch_array($likesql);
+        // var_dump($status);
+        // var_dump($userid);
+        // var_dump($_SESSION['userid']);
+        // var_dump($komenid);
+
+        // KOMPARASI Status , userid, komenid
+        if (isset($likekomen)) {
+            if ($likekomen['status'] == 1 && $likekomen['userid'] === $_SESSION['userid']) {
+                echo '
                             <div class="col-1 text-end">
                                 <span class="liked">
                                     </br>
                                     <form action="./controller/like.php" method="POST">
-                                    <input type="hidden" name="idberita" value='.$idberita.'>
-                                    <input type="hidden" name="idkomen" value='.$idkomen.'>
+                                    <input type="hidden" name="idberita" value=' . $idberita . '>
+                                    <input type="hidden" name="idkomen" value=' . $idkomen . '>
                                     <button class="likebutton" type="submit" name="like"><i class="bi bi-heart-fill"></i></button>
                                     </form> 
                                 </span>';
-                } else {
-                echo'
+            } else {
+                echo '
                         <div class="col-1 text-end">
                             <span class="like">
                                 </br>
                                 <form action="./controller/like.php" method="POST">
-                                <input type="hidden" name="idberita" value='.$idberita.'>
-                                <input type="hidden" name="idkomen" value='.$idkomen.'>
+                                <input type="hidden" name="idberita" value=' . $idberita . '>
+                                <input type="hidden" name="idkomen" value=' . $idkomen . '>
                                 <button class="likebutton" type="submit" name="like"><i class="bi bi-heart-fill"></i></button>
                                 </form> 
                             </span>';
-                }
-            } else {
-                echo'
+            }
+        } else {
+            echo '
                 <div class="col-1 text-end">
                     <span class="like">
                         </br>
                         <form action="./controller/like.php" method="POST">
-                        <input type="hidden" name="idberita" value='.$idberita.'>
-                        <input type="hidden" name="idkomen" value='.$idkomen.'>
+                        <input type="hidden" name="idberita" value=' . $idberita . '>
+                        <input type="hidden" name="idkomen" value=' . $idkomen . '>
                         <button class="likebutton" type="submit" name="like"><i class="bi bi-heart-fill"></i></button>
                         </form> 
                     </span>';
-            }
+        }
 
 
-            echo'
+        echo '
                     </div>
                 </div>
                     
 
                 <div class="clear"></div>
             ';
+    }
+    ?>
 
-            }
-        ?>
-
-	<div class="clear"></div>
+    <div class="clear"></div>
 
 </div>
