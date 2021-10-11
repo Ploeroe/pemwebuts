8aa55c5eeaf5ff25ffd987723c732849b31c5be5
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-10-11 20:33:27 +0700

    benerin bug

--- admin/kategori.php
+++ admin/kategori.php
@@ -37,8 +37,6 @@ if (isset($_GET['act']) && $_GET['act'] == 'hapus') {
 	global $connect;
 
 	$sql = mysqli_query($connect, "DELETE FROM kategori WHERE ID = '$id' ");
-
-	header('Location:./?mod=kategori');
 }
 
 ?>
